986e55013ff2bb9bbefaeaa3411eac6f2423793c
Ghazi Omar <ghazi.omar@gmail.com>
2016-06-29 18:24:23 -0400

    making sure the additions are added

--- modules/cooks/server/models/cook.server.model.js
+++ modules/cooks/server/models/cook.server.model.js
@@ -22,6 +22,48 @@ var CookSchema = new Schema({
     required: 'Please fill Cooks background story',
     trim: true
   },
+  address: {
+    type: String,
+    default: '',
+    required: 'Please provide your address',
+    trim: true
+  },
+  phone: {
+    type: String,
+    default: '',
+    required: 'Please provide your phone number',
+    trim: true,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: '{VALUE} is not a valid phone number! example: 571-123-4567'
+    }
+  },
+  foodLimit: {
+    type: Number,
+    default: '',
+    required: 'What is the maximum number of orders you can service at one time?',
+    min: 1,
+    trim: true
+  },
+  specialties: {
+    type: String,
+    default: '',
+    required: 'Do you have any specialities?',
+    trim: true
+  },
+  cookingPic: {
+    type: String,
+    default: 'modules/users/client/img/profile/default.png',
+    required: 'Please Upload a picture of your kitchen or you cooking'
+  },
+  profile: {
+    type: String,
+    default: '',
+    required: 'Please provide overview of your cooking style or restrictions',
+    trim: true
+  },
   created: {
     type: Date,
     default: Date.now
