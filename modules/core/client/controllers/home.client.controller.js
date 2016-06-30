4729cea812615406c79be0e76c156e6716443099
Sonny Khan <gr8khan@gmail.com>
2016-06-30 05:11:51 -0400

    Main page fixed and added missing css for it

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -8,22 +8,22 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     var cooks = [
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 1',
-        image:'http://placehold.it/300x200?text=food1'},
+        image:'http://placehold.it/350x200?text=food1'},
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 2',
-        image:'http://placehold.it/300x200?text=food2' },
+        image:'http://placehold.it/350x200?text=food2' },
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 3',
-        image:'http://placehold.it/300x200?text=food3' },
+        image:'http://placehold.it/350x200?text=food3' },
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 4',
-        image:'http://placehold.it/300x200?text=food4' },
+        image:'http://placehold.it/350x200?text=food4' },
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 5',
-        image:'http://placehold.it/300x200?text=food5' },
+        image:'http://placehold.it/350x200?text=food5' },
       { profilePhoto: 'http://placehold.it/50x50',
         name: 'Cook 6',
-        image:'http://placehold.it/300x200?text=food6' }
+        image:'http://placehold.it/350x200?text=food6' }
     ];
 
     //var cuisines = [
